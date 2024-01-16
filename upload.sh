9697aed149bd407be9788cad943608dca4d9b861
ElementalMP4 <sebahudl@gmail.com>
2024-01-16 22:28:06 +0000

    Make things more easily modifiable

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 SERVER_SSH_ADDRESS="wyse"
+SERVER_REMOTE_USER="elementalmp4"
 
-rsync -av -e ssh --exclude='node_modules' --exclude='.git' . elementalmp4@${SERVER_SSH_ADDRESS}:/home/elementalmp4/Techathon2024
+rsync -av -e ssh --exclude='node_modules' --exclude='.git' . ${SERVER_REMOTE_USER}@${SERVER_SSH_ADDRESS}:/home/${SERVER_REMOTE_USER}/Techathon2024
