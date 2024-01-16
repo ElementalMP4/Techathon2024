8c3b4332e4b818026d0bceaf32ec1e1935a1727e
ElementalMP4 <sebahudl@gmail.com>
2024-01-16 21:55:28 +0000

    Add the server

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const morgan = require('morgan');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server: server, path: "/gateway" });
+
+const connections = new Map();
+
+app.use(morgan('dev'));
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.redirect("/index.html");
+});
+
+wss.on('connection', (ws) => {
+    const id = uuidv4();
+    console.log(`New connection ${id}`);
+
+    ws.send("Hello Client");
+
+    connections.set(id, ws);
+    ws.on('message', (data) => {
+        console.log(`${id}: ${data}`);
+    });
+
+    ws.on('close', () => {
+        console.log(`Connection to ${id} lost`);
+        connections.delete(id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
