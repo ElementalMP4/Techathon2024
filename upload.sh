abed9829e356c015a5928975a6699307d0433752
ElementalMP4 <sebahudl@gmail.com>
2024-01-16 22:26:50 +0000

    Infrastructure

--- upload.sh
+++ upload.sh
@@ -1,1 +1,3 @@
-rsync -av -e ssh --exclude='node_modules' --exclude='.git' . elementalmp4@wyse:/home/elementalmp4/Techathon2024
+SERVER_SSH_ADDRESS="wyse"
+
+rsync -av -e ssh --exclude='node_modules' --exclude='.git' . elementalmp4@${SERVER_SSH_ADDRESS}:/home/elementalmp4/Techathon2024
