c583cf88e88f73571231d78b5a70fc532772c0b1
ElementalMP4 <sebahudl@gmail.com>
2024-02-20 12:59:52 +0000

    Build for multiple platforms

--- MicroNetGo/build.sh
+++ MicroNetGo/build.sh
@@ -1,2 +1,8 @@
-GOOS=windows GOARCH=amd64 go build -o bin/micronet.exe
-GOOS=darwin GOARCH=arm64 go build -o bin/micronet
+echo "Compiling for Windows"
+GOOS=windows GOARCH=amd64 go build -o bin/micronet_windows_amd64.exe
+echo "Compiling for MacOS"
+GOOS=darwin GOARCH=arm64 go build -o bin/micronet_macos_arm
+echo "Compiling for Linux ARM"
+GOOS=linux GOARCH=arm64 go build -o bin/micronet_linux_arm64
+echo "Compiling for Linux x86"
+GOOS=linux GOARCH=amd64 go build -o bin/micronet_linux_amd64
